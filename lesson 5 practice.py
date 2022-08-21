d22081e99240731e84fd9700ac091a8df2931807
PashaShevtsov <balotelli357952@gmail.com>
2022-08-21 15:32:05 +0300

    commen

---
+++ lesson 5 practice.py
@@ -0,0 +1,10 @@
+#-----task1-----
+def func1(string):
+    return string.lower()
+
+def func2(string):
+    return string.upper()
+
+#-----task2-----
+def squres(x):
+    squares = list(map(lambda x: x * x, range(2, 101)))
